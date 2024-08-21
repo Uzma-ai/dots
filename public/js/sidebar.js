c154dd703b00fa2b69ecce05f9360a83c7057296
pooja <thakarepooja11@gmail.com>
2024-08-21 22:24:06 +0545

    active class changes

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,14 +1,13 @@
 
 // manage sidebar
-const currentPath = window.location.pathname.split("/").pop();
-
-const sidebarLinks = document.querySelectorAll('.sidebar a');
-// find the link whose href === currentPath and apply 'active' class, break on first match
+const currentPath = window.location.pathname.split("/").pop() || 'dashboard';
+    const sidebarLinks = document.querySelectorAll('.sidebar a');
 for (let i = 0; i < sidebarLinks.length; i++) {
     const link = sidebarLinks[i];
-    if (link.getAttribute('href') === currentPath) {
+    const linkPath = link.getAttribute('href').split("/").pop() || 'dashboard';
+    if (linkPath === currentPath) {
+        // console.log(currentPath, linkPath);
         link.classList.add('active');
-        // find the closest parent div with 'drop-menu' class
         const dropMenu = link.closest('.drop-menu');
         if (dropMenu) {
             dropMenu.classList.add('active');
