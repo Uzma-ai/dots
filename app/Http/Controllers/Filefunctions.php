d13f41ff904b429dcaa8cd6461555f10cf447908
Mona <monig5658@gmail.comm>
2024-08-24 12:47:17 +0530

    Iframe Bugs

---
+++ app/Http/Controllers/Filefunctions.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+use App\Models\File as FileModel ;
+use App\Models\LightApp;
+
+class Filefunctions extends Controller
+{
+    public function createNewFile($fileatype, $destinationParentPath){
+        $destinationPath = Storage::disk('root')->path($destinationParentPath);
+        $sourcePath = Storage::disk('public')->path('newfile.'.$fileatype);
+        $newFileName = 'New File.'.$fileatype;
+        $actualpath = $destinationParentPath.'/'.$newFileName;
+        $count = 1;
+        $destinationfPath = $destinationPath.'/'.$newFileName;
+        while (file_exists($destinationfPath)) {
+            $newFileName = 'New File('.($count).').'.$fileatype;
+            $destinationfPath = $destinationPath.'/'.$newFileName;
+            $actualpath = $destinationParentPath.'/'.$newFileName;
+            $count++;
+        }
+        
+        if($fileatype=='pptx'){
+            $checkapp = 'PPT';
+        }else if( $fileatype=='xlsx'){
+            $checkapp = 'EXCEL';
+        }else{
+            $checkapp = 'Docx';
+        }
+        $lightapp = LightApp::where('name',$checkapp)->where('status',1)->first();
+        if (copy($sourcePath, $destinationfPath)) {
+            $filetype = $this->getFiletype($destinationfPath);
+            $newFile = new FileModel();
+            $newFile->name = $newFileName;
+            $newFile->extension = $fileatype;
+            $newFile->filetype = $filetype;
+            $newFile->parentpath = $destinationParentPath;
+            $newFile->path = $actualpath;
+            $newFile->openwith = ($lightapp) ? $lightapp->id : '';
+            $newFile->status = 1; // Assuming 1 means active
+            $newFile->created_by = auth()->id(); // Assuming you want to save the ID of the authenticated user
+            $newfilekey = $newFile->save();
+            $appname =($lightapp) ? $lightapp->name :'';
+            $appicon =($lightapp) ? $lightapp->icon :'';
+            $returnarr = array('filekey'=> $newfilekey,'appkey'=> $newFile->openwith,'filename'=>$newFile->name,'appname'=>$appname,'appicon'=>$appicon);
+            return $returnarr;
+
+        }else{
+            return false;
+        }
+    }
+
+    public function getDocumentType($ext) {
+        $ExtsDoc = array("doc", "docm", "docx", "dot", "dotm", "dotx", "epub", "fodt", "ott", "htm", "html", "mht", "odt", "pdf", "rtf", "txt", "djvu", "xps");
+        $ExtsPre = array("fodp", "odp", "pot", "potm", "potx", "pps", "ppsm", "ppsx", "ppt", "pptm", "pptx", "otp");
+        $ExtsSheet = array("xls", "xlsx", "xltx", "ods", "ots", "csv", "xlt", "xltm", "fods");
+        if (in_array($ext,$ExtsDoc)) {
+            return "word";
+        } elseif (in_array($ext,$ExtsPre)) {
+            return "presentation";
+        } elseif (in_array($ext,$ExtsSheet)) {
+            return "spreadsheet";
+        } else {
+            return "undefined";
+        }
+    }
+    
+    public function fileTypeAlias($ext) {
+        if (strpos(".docm.dotm.dot.wps.wpt",'.'.$ext) !== false) {
+            $ext = 'doc';
+        } else if (strpos(".xlt.xltx.xlsm.dotx.et.ett",'.'.$ext) !== false) {
+            $ext = 'xls';
+        } else if (strpos(".pot.potx.pptm.ppsm.potm.dps.dpt",'.'.$ext) !== false) {
+            $ext = 'ppt';
+        }
+        return $ext;
+    }
+    
+    public function getFiletype($file){
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $mime = finfo_file($finfo, $file);
+        finfo_close($finfo);
+        $mimeParts = explode('/', $mime);
+        return $mimeParts[0];
+    }
+
+    public function getFileSize($filePath)
+    {
+        if (File::exists($filePath)) {
+            $size = File::size($filePath);
+            return $size;
+        } else {
+            return false;
+        }
+    }
+    
+    private function folderSize($directory)
+    {
+        $size = 0;
+        foreach (File::allFiles($directory) as $file) {
+            $size += $file->getSize();
+        }
+        return $size;
+    }
+}
