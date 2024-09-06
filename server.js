73ba1097b2f7e04d454f42173f0d911cc27194a4
Mayank Bhandure <mayu.bhandure657@gmail.com>
2024-09-06 16:30:37 +0530

    updated node code

--- server.js
+++ server.js
@@ -2,36 +2,47 @@ import express from 'express';
 import { createServer } from 'node:http';
 import { Server } from 'socket.io';
 import cors from 'cors';
+import http from "http";
 
-const app = express();
-app.use(express.json());
-const server = createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: true,
-        methods: ["GET", "POST"]
-    }
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello World\n");
 });
 
-
-app.get('/node', (req, res) => {
-    res.send('<h1>Hello world</h1>');
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
 });
+// const app = express();
+// app.use(express.json());
+// const server = createServer(app);
+// const io = new Server(server, {
+//     cors: {
+//         origin: true,
+//         methods: ["GET", "POST"]
+//     }
+// });
 
-app.post('/sendNotice', (req, res) => {
-    const { user, data } = req.body;
-    io.emit('receiveNotificationToUser_' + user, data);
-    res.sendStatus(200);
-});
 
-io.on('connection', (socket) => {
-    socket.on('sendNotice', (obj) => {
-        socket.broadcast.emit('receiveNotificationToUser_' + obj.user, obj.data);
-    });
-});
+// app.get('/node', (req, res) => {
+//     res.send('<h1>Hello world</h1>');
+// });
 
-const port = 3000;
+// app.post('/sendNotice', (req, res) => {
+//     const { user, data } = req.body;
+//     io.emit('receiveNotificationToUser_' + user, data);
+//     res.sendStatus(200);
+// });
 
-server.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
-});
+// io.on('connection', (socket) => {
+//     socket.on('sendNotice', (obj) => {
+//         socket.broadcast.emit('receiveNotificationToUser_' + obj.user, obj.data);
+//     });
+// });
+
+// const port = 3000;
+
+// server.listen(port, () => {
+//     console.log(`Server running on http://localhost:${port}`);
+// });
