eb4e0fa7efdc13e3b0d65002d7ffb226ebd2b695
Mayank Bhandure <mayu.bhandure657@gmail.com>
2024-09-10 12:46:32 +0530

    changes in jobs

--- app/Jobs/SendNoticeJob.php
+++ app/Jobs/SendNoticeJob.php
@@ -42,14 +42,7 @@ class SendNoticeJob implements ShouldQueue
                     'user' => $userId,
                     'data' => $notice
                 ];
-                // Http::post($this->url, $data);
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_URL, $this->url);
-                curl_setopt($ch, CURLOPT_POST, 1);
-                curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                $response = curl_exec($ch);
-                curl_close($ch);
+                Http::post($this->url, $data);
             }
         }
     }
